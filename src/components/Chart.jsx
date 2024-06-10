c1df9c8dc2a98755267159fd1398cccaec24ad18
Adem Bouatay <adem.bouatay@eniso.u-sousse.tn>
2024-06-10 21:05:34 +0100

    selection sort

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -54,20 +54,72 @@ const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-const Chart = ({ initialData, labels }) => {
+const Chart = ({ initialData }) => {
   const [cursor, setCursor] = useState([0, 1]);
   const [sortedData, setSortedData] = useState(initialData);
   const [log, setLog] = useState(
     `original array = [${initialData.join(", ")}]\n`
   );
+  const selectionSort = async () => {
+    const array = [...sortedData];
+    let newLog = log;
+    let index = 0;
+    while (index < array.length - 1) {
+      let min = index;
+      for (let i = index + 1; i < array.length; i++) {
+        setCursor([index, i]);
+        await sleep(200);
+        if (array[i] < array[min]) min = i;
+      }
+      const temp = array[index];
+      array[index] = array[min];
+      array[min] = temp;
+      setSortedData([...array]);
+      newLog += `Swapped ${array[index]} and ${array[min]}\n`;
+      setLog(newLog);
+      index++;
+    }
+    setCursor([]);
+  };
+  const bubbleSort = async () => {
+    const array = [...sortedData];
+    let change = true;
+    let newLog = log;
 
-  const cursorColor = (context) => {
+    while (change) {
+      change = false;
+      for (let i = 0; i < array.length - 1; i++) {
+        setCursor([i, i + 1]);
+        await sleep(1000);
+        if (array[i] > array[i + 1]) {
+          const temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+          change = true;
+          setSortedData([...array]);
+          newLog += `Swapped ${array[i]} and ${array[i + 1]}\n`;
+          setLog(newLog);
+        }
+      }
+    }
+    setCursor([]);
+  };
+
+  /* const cursorColor = (context) => {
     const index = context.dataIndex;
     return cursor.includes(index) ? "#ffff" : "#f472b6";
+  };*/
+  const cursorColor = (context) => {
+    const index = context.dataIndex;
+    return index === cursor[0]
+      ? "#00a6a0"
+      : index === cursor[1]
+      ? "#ffff"
+      : "#f472b6";
   };
 
   const [data, setData] = useState({
-    labels,
+    labels: sortedData,
     datasets: [
       {
         label: "Dataset",
@@ -78,35 +130,13 @@ const Chart = ({ initialData, labels }) => {
   });
 
   useEffect(() => {
-    const bubbleSort = async () => {
-      const array = [...sortedData];
-      let change = true;
-      let newLog = log;
-
-      while (change) {
-        change = false;
-        for (let i = 0; i < array.length - 1; i++) {
-          setCursor([i, i + 1]);
-          await sleep(1000);
-          if (array[i] > array[i + 1]) {
-            const temp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = temp;
-            change = true;
-            setSortedData([...array]);
-            newLog += `Swapped ${array[i]} and ${array[i + 1]}\n`;
-            setLog(newLog);
-          }
-        }
-      }
-      setCursor([]);
-    };
-    bubbleSort();
+    //bubbleSort();
+    selectionSort();
   }, []);
 
   useEffect(() => {
     setData((prevData) => ({
-      ...prevData,
+      labels: sortedData,
       datasets: prevData.datasets.map((dataset) => ({
         ...dataset,
         data: sortedData,
@@ -127,7 +157,9 @@ const Chart = ({ initialData, labels }) => {
   return (
     <>
       <Bar options={options} data={data} updateMode="active" />
-      <h1 className="text-lg font-bold text-[#646464]">Log:</h1>
+      <h1 className="text-lg font-bold text-[#646464]" onClick={selectionSort}>
+        Log:
+      </h1>
       <div className="w-full h-full">
         <textarea
           className="w-full h-full bg-[#151515] resize-none p-3"
