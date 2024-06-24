1a80ce9c1abff8dedb6ca36cdcd35c0b5b7e0eaf
Adem-Bouatay <ademessadem@gmail.com>
2024-06-24 12:57:04 +0100

    added linear search

--- src/app/search/linear/page.js
+++ src/app/search/linear/page.js
@@ -1,17 +1,90 @@
 "use client";
 
-import React from "react";
-import Chart from "@/components/BubbleChart";
+import React, { useState, useEffect } from "react";
+import Chart from "@/components/Chart";
 import { faker } from "@faker-js/faker";
 
 const labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const data = labels.map(() => faker.number.int({ min: 0, max: 100 }));
+const initialData = labels.map(() => faker.number.int({ min: 0, max: 100 }));
 
 const App = () => {
+  const [number, setNumber] = useState(0);
+  const [cursor, setCursor] = useState([0]);
+  const [sortedData, setSortedData] = useState(initialData);
+
+  const linearSearch = async () => {
+    const array = [...sortedData];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === number) {
+        setCursor([i]);
+        await sleep(300);
+        alert(`Number found at index ${i}`);
+        return;
+      }
+      setCursor([i]);
+      await sleep(300);
+    }
+    setCursor([]);
+  };
+
+  const cursorColor = (context) => {
+    const index = context.dataIndex;
+    return cursor.includes(index) ? "#ffff" : "#f472b6";
+  };
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const [data, setData] = useState({
+    labels: sortedData,
+    datasets: [
+      {
+        label: "Dataset",
+        data: sortedData,
+        backgroundColor: cursorColor,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    setData((prevData) => ({
+      labels: sortedData,
+      datasets: prevData.datasets.map((dataset) => ({
+        ...dataset,
+        data: sortedData,
+      })),
+    }));
+  }, [sortedData]);
+
+  useEffect(() => {
+    setData((prevData) => ({
+      ...prevData,
+      datasets: prevData.datasets.map((dataset) => ({
+        ...dataset,
+        backgroundColor: cursorColor,
+      })),
+    }));
+  }, [cursor]);
   return (
     <>
       <div className="flex flex-col w-5/12 p-5 space-y-4 items-center">
-        <Chart initialData={data} labels={data} />
+        <button
+          className="bg-slate-50 text-black rounded-lg p-2 text-sm font-semibold hover:bg-slate-300 active:bg-red-400"
+          onClick={linearSearch}
+        >
+          ►
+        </button>
+        <Chart data={data} />
+        <h1 className="text-lg font-bold text-[#646464]">
+          Number to search for:
+        </h1>
+        <input
+          className="w-1/2 p-2 rounded-lg text-center text-black"
+          type="number"
+          placeholder="Enter a number"
+          onChange={(e) => setNumber(parseInt(e.target.value))}
+        />
       </div>
       <div className="flex-1 p-5 border-l-2 rounded-l-3xl text-white">
         <textarea
---
+++ src/components/Chart.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Bubble Sort",
+      font: {
+        size: 18,
+        weight: "bold",
+      },
+    },
+    legend: {
+      display: false,
+    },
+  },
+  responsive: true,
+};
+
+const Chart = ({ data }) => {
+  return (
+    <>
+      <Bar options={options} data={data} updateMode="active" />
+    </>
+  );
+};
+
+export default Chart;
