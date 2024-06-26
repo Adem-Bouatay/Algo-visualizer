30975ccf14e39d13989a76343f4b04cdd0e5ccb5
Adem Bouatay <adem.bouatay@eniso.u-sousse.tn>
2024-06-26 16:24:10 +0100

    updated the sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -17,31 +17,8 @@ const App = () => {
     <>
       <main className="flex flex-row">
         <SideBar />
-        <div className="flex flex-col w-5/12 p-5 space-y-4 items-center">
-          <Chart initialData={data} labels={data} />
-        </div>
-        <div className="flex-1 p-5 border-l-2 rounded-l-3xl text-white">
-          <textarea
-            className="w-full h-full resize-none p-2 bg-[#151515]"
-            disabled
-            defaultValue={`const array = [...sortedData];
-              let change = true;
-              while (change) {
-                change = false;
-                for (let i = 0; i < array.length - 1; i++) {
-                  setCursor([i, i + 1]);
-                  await sleep(200);
-                  if (array[i] > array[i + 1]) {
-                    const temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-                    change = true;
-                    setSortedData([...array]);
-                  }
-                }
-              }
-            `}
-          />
+        <div className="flex place-content-center items-center w-full">
+          Home Page
         </div>
       </main>
     </>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 const SideBar = () => {
+  const [isSortOpen, setIsSortOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
   return (
     <div className="h-svh min-w-80 flex flex-col items-center bg-slate-100 text-black">
       <div className="w-11/12 items-center divide-y py-5 space-y-5 divide-gray-900/10">
@@ -8,54 +13,108 @@ const SideBar = () => {
           Algo Visualizer
         </h1>
         <div className="flex flex-col space-y-2 font-medium">
-          <Link
-            href="/sort/bubble"
-            className="p-2 text-center mt-5 hover:bg-pink-300 rounded-md "
-          >
-            Bubble Sort
-          </Link>
-          <Link
-            href="/sort/selection"
-            className="p-2 text-center hover:bg-pink-300 rounded-md"
-          >
-            Selection Sort
-          </Link>
-          <Link
-            href="/sort/insertion"
-            className="p-2 text-center hover:bg-pink-300 rounded-md"
-          >
-            Insertion Sort
-          </Link>
-          <Link
-            href="/sort/merge"
-            className="p-2 text-center hover:bg-pink-300 rounded-md"
-          >
-            Merge Sort
-          </Link>
-          <Link
-            href="/sort/quick"
-            className="p-2 text-center hover:bg-pink-300 rounded-md"
-          >
-            Quick Sort
-          </Link>
-          <Link
-            href="/search/linear"
-            className="p-2 text-center hover:bg-blue-300 rounded-md"
-          >
-            Linear Search
-          </Link>
-          <Link
-            href="/search/binary"
-            className="p-2 text-center hover:bg-blue-300 rounded-md"
-          >
-            Binary Search
-          </Link>
-          <Link
-            href="/search/ternary"
-            className="p-2 text-center hover:bg-blue-300 rounded-md"
-          >
-            Ternary Search
-          </Link>
+          <div className="flex-col py-2">
+            <button
+              type="button"
+              class="flex w-full items-center justify-between rounded-lg"
+              aria-controls="disclosure-1"
+              aria-expanded="false"
+              onClick={() => setIsSortOpen(!isSortOpen)}
+            >
+              Sorting Algorithms
+              <svg
+                className={`h-5 w-5 flex-none ${
+                  isSortOpen ? "rotate-180 duration-300" : " duration-300"
+                }`}
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+            <div className={`${isSortOpen ? "" : "hidden"} flex flex-col`}>
+              <Link
+                href="/sort/bubble"
+                className="p-2 text-center mt-5 hover:bg-pink-300 rounded-md "
+              >
+                Bubble Sort
+              </Link>
+              <Link
+                href="/sort/selection"
+                className="p-2 text-center hover:bg-pink-300 rounded-md"
+              >
+                Selection Sort
+              </Link>
+              <Link
+                href="/sort/insertion"
+                className="p-2 text-center hover:bg-pink-300 rounded-md"
+              >
+                Insertion Sort
+              </Link>
+              <Link
+                href="/sort/merge"
+                className="p-2 text-center hover:bg-pink-300 rounded-md"
+              >
+                Merge Sort
+              </Link>
+              <Link
+                href="/sort/quick"
+                className="p-2 text-center hover:bg-pink-300 rounded-md"
+              >
+                Quick Sort
+              </Link>
+            </div>
+          </div>
+          <div className="flex-col py-2">
+            <button
+              type="button"
+              class="flex w-full items-center justify-between rounded-lg"
+              aria-controls="disclosure-2"
+              aria-expanded="false"
+              onClick={() => setIsSearchOpen(!isSearchOpen)}
+            >
+              Searching Algorithms
+              <svg
+                className={`h-5 w-5 flex-none ${
+                  isSearchOpen ? "rotate-180 duration-300" : " duration-300"
+                }`}
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+            <div className={`${isSearchOpen ? "" : "hidden"} flex flex-col`}>
+              <Link
+                href="/search/linear"
+                className="p-2 text-center hover:bg-blue-300 rounded-md"
+              >
+                Linear Search
+              </Link>
+              <Link
+                href="/search/binary"
+                className="p-2 text-center hover:bg-blue-300 rounded-md"
+              >
+                Binary Search
+              </Link>
+              <Link
+                href="/search/ternary"
+                className="p-2 text-center hover:bg-blue-300 rounded-md"
+              >
+                Ternary Search
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
